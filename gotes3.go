6f0d4000d31c668c2ba07f915d767deda8e8f077
Aaron <alpha_963@163.com>
2015-10-21 14:12:26 +0800

    test for

--- gotes3.go
+++ gotes3.go
@@ -6,16 +6,23 @@ import (
 	"math"
 )
 
+func sqrt(x float64) string {
+	if x < 0 {
+		return sqrt(-x) + "i"
+	}
+	return fmt.Sprint(math.Sqrt(x))
+}
+
 func main() {
-	var x, y int = 3, 4
+	var x, y,sum,i int = 3, 4,0,1
 	var f float64 = math.Sqrt(float64(x*x + y*y))
 	var z int = int(f)
 	fmt.Println(x, y, z)
 	fmt.Printf("f and z is of type %T,%T\n", f,z)
 
-	sum:=0
-	for i:=0;i<=101;i++{
+	for ;i<=100;i++ {
 	sum+=i
 	}
 	fmt.Println(sum)
+	fmt.Println(sqrt(2), sqrt(-4))
 }
